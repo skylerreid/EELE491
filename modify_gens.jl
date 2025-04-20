e25c961215dc8b21e2478556eb740f9072ceb77d
Skyler Reid <96967651+skylerreid@users.noreply.github.com>
2025-04-20 14:54:33 -0600

    Update modify_gens.jl
    added description on how to use

--- modify_gens.jl
+++ modify_gens.jl
@@ -1,3 +1,7 @@
+#like the modify_loads function, this can be called inline on a case defined using the PowerModels.jl parser
+#instead of specifying an area to operate over, this function works on bus numbers since there are typically far fewer gens than load buses. 
+#unlike the modify_loads function, p and q can be adjusted separately
+
 function modify_gens(case::Dict, gen_indices::Vector{Int}, p_multiplier::Float64, q_multiplier::Float64)
     println("Adjusted generators:")
     for gen_index in gen_indices
