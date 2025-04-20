ba7e5000da37dec69e099fd824ba0514bbf191ff
Skyler Reid <96967651+skylerreid@users.noreply.github.com>
2025-04-20 14:52:05 -0600

    Update modify_loads.jl
    added description for how to use this function

--- modify_loads.jl
+++ modify_loads.jl
@@ -1,3 +1,7 @@
+#this function can be used to modify the P and Q values at buses from a network case read in by the PowerModels.jl parser. currently, both the P and Q are multiplied by the same value. 
+#call this function inline, after defining a case using "case = parse_file(case_path)"
+#the coordinates describe a rectangle around an area. any bus in this geographic region will be adjusted. 
+
 function modify_loads(case::Dict, lat_min::Float64, lat_max::Float64, long_min::Float64, long_max::Float64, multiplier::Float64)
     println("Adjusted buses:")
     for (bus_num, bus_data) in case["bus"]
